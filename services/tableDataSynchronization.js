6afb80a396e2575ce7057378498577db2e18beee
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-06-27 09:21:49 -0300

    archive sicronization table

---
+++ services/tableDataSynchronization.js
@@ -0,0 +1,47 @@
+setInterval(fetchData, 1000); 
+document.addEventListener('DOMContentLoaded', function () {
+  fetchData();
+});
+
+function fetchData() {
+  fetch('http://localhost:3000/patients') // URL para buscar todos os pacientes
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok ' + response.statusText);
+          }
+          return response.json();
+      })
+      .then(data => renderTable(data))
+      .catch(error => console.error('Erro ao buscar dados:', error));
+}
+
+function renderTable(data) {
+  const tableBody = document.querySelector('.patients-list tbody');
+  tableBody.innerHTML = ''; // Limpa o corpo da tabela antes de adicionar os dados
+
+  data.forEach(item => {
+      const row = document.createElement('tr');
+      
+      const cellId = document.createElement('td');
+      cellId.textContent = item.id;
+      row.appendChild(cellId);
+
+      const cellName = document.createElement('td');
+      cellName.textContent = item.name;
+      cellName.colSpan = 8; // Adiciona o atributo colspan
+      row.appendChild(cellName);
+
+      const cellSex = document.createElement('td');
+      cellSex.textContent = item.sex;
+      row.appendChild(cellSex);
+
+      const cellStatus = document.createElement('td');
+      cellStatus.textContent = item.status;
+      row.appendChild(cellStatus);
+
+      tableBody.appendChild(row);
+  });
+}
+
+
+
