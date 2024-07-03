a26c7f56e2e809ca29c6be157890aac1d03ce9a5
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-07-03 09:13:39 -0300

    adding stylizations and function modal for form

--- services/tableDataSynchronization.js
+++ services/tableDataSynchronization.js
@@ -41,3 +41,58 @@ function renderTable(data) {
 }
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.querySelector('.btn-add');
+    const viewButton = document.querySelector('.btn-view');
+    const updateButton = document.querySelector('.btn-update');
+    const deleteButton = document.querySelector('.btn-delete');
+  
+    const addModal = document.getElementById('formAdd');
+    const viewModal = document.getElementById('formView');
+    const updateModal = document.getElementById('formUpdate');
+    const deleteModal = document.getElementById('formDelete');
+  
+    const closeButtons = document.querySelectorAll('.close');
+  
+    addButton.addEventListener('click', () => {
+      addModal.classList.remove('hidden');
+      addModal.style.display = 'block';
+    });
+  
+    viewButton.addEventListener('click', () => {
+      viewModal.classList.remove('hidden');
+      viewModal.style.display = 'block';
+    });
+  
+    updateButton.addEventListener('click', () => {
+      updateModal.classList.remove('hidden');
+      updateModal.style.display = 'block';
+    });
+  
+    deleteButton.addEventListener('click', () => {
+      deleteModal.classList.remove('hidden');
+      deleteModal.style.display = 'block';
+    });
+  
+    closeButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        addModal.style.display = 'none';
+        viewModal.style.display = 'none';
+        updateModal.style.display = 'none';
+        deleteModal.style.display = 'none';
+      });
+    });
+  
+    window.addEventListener('click', (event) => {
+      if (event.target == addModal) {
+        addModal.style.display = 'none';
+      } else if (event.target == viewModal) {
+        viewModal.style.display = 'none';
+      } else if (event.target == updateModal) {
+        updateModal.style.display = 'none';
+      } else if (event.target == deleteModal) {
+        deleteModal.style.display = 'none';
+      }
+    });
+  });
+    
